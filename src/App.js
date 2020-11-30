1c93186bdaca2e9c4222f560249420154a255cc5
Nigel <nigel.patching@gmail.com>
2020-11-30 09:53:03 +0000

    Add copy to clipboard

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Palette from "./Palette";
-import seedColors from "./seedColors";
+import seedColors from "./SeedColors";
 
 class App extends Component {
   render() {
