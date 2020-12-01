7ffb28981408a12bbe6c7385fa55bfedcbeaa122
Nigel <nigel.patching@gmail.com>
2020-12-01 22:42:21 +0000

    Add footer

--- src/Palette.js
+++ src/Palette.js
@@ -17,10 +17,10 @@ class Palette extends Component {
     this.setState({ format });
   }
   render() {
-    const { colors } = this.props.palette;
+    const { colors, paletteName, emoji } = this.props.palette;
     const { level, format } = this.state;
     const colorBoxes = colors[level].map((color) => (
-      <ColorBox background={color[format]} name={color.name} />
+      <ColorBox background={color[format]} name={color.name} key={color.id} />
     ));
     return (
       <div className='Palette'>
@@ -30,6 +30,10 @@ class Palette extends Component {
           handleFormatChange={this.changeFormat}
         />
         <div className='Palette-colors'>{colorBoxes}</div>
+        <footer className='Palette-footer'>
+          {paletteName}
+          <span className='emoji'>{emoji}</span>
+        </footer>
       </div>
     );
   }
