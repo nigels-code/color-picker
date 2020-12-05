4ac29edb7d1193fdadf011277064982126cc3ed9
Nigel <nigel.patching@gmail.com>
2020-12-05 16:57:22 +0000

    Add ColorPicker

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -8,11 +8,13 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
 import IconButton from "@material-ui/core/IconButton";
+import Button from "@material-ui/core/Button";
 import MenuIcon from "@material-ui/icons/Menu";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import { withStyles } from "@material-ui/styles";
+import { ChromePicker } from "react-color";
 
-const drawerWidth = 240;
+const drawerWidth = 400;
 
 const styles = makeStyles((theme) => ({
   root: {
@@ -123,6 +125,19 @@ function NewPaletteForm() {
           </IconButton>
         </div>
         <Divider />
+        <Typography variant='h4'>Design Your Palette</Typography>
+        <div>
+          <Button variant='contained' color='secondary'>
+            Clear Palette
+          </Button>
+          <Button variant='contained' color='primary'>
+            Random Color
+          </Button>
+        </div>
+        <ChromePicker
+          color='red'
+          onChangeComplete={(newColor) => console.log(newColor)}
+        />
       </Drawer>
       <main
         className={clsx(classes.content, {
