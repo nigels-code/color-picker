c5411cac3abd5da14ac7076aec263a9434bafc53
Nigel <nigel.patching@gmail.com>
2020-12-16 13:08:46 +0000

    Finish MiniPalette delete

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -4,7 +4,11 @@ import styles from "./styles/MiniPaletteStyles";
 import DeleteIcon from "@material-ui/icons/Delete";
 
 function MiniPalette(props) {
-  const { classes, paletteName, emoji, colors } = props;
+  const { classes, paletteName, deletePalette, id, emoji, colors } = props;
+  const handleDelete = (evt) => {
+    evt.stopPropagation();
+    deletePalette(id);
+  };
   const miniColorBoxes = colors.map((color) => (
     <div
       className={classes.miniColor}
@@ -14,12 +18,11 @@ function MiniPalette(props) {
   ));
   return (
     <div className={classes.root} onClick={props.handleClick}>
-      <div className={classes.delete}>
-        <DeleteIcon
-          className={classes.deleteIcon}
-          style={{ transition: "all 0.3s ease-in-out" }}
-        />
-      </div>
+      <DeleteIcon
+        className={classes.deleteIcon}
+        style={{ transition: "all 0.3s ease-in-out" }}
+        onClick={handleDelete}
+      />
       <div className={classes.colors}>{miniColorBoxes}</div>
       <h5 className={classes.title}>
         {paletteName} <span className={classes.emoji}>{emoji}</span>
