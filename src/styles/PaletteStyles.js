62a5d86cb637e45276cc655c0e3f99b588a68b9a
Nigel <75135800+nigels-code@users.noreply.github.com>
2021-08-05 21:49:43 +0100

    Update PaletteStyles

--- src/styles/PaletteStyles.js
+++ src/styles/PaletteStyles.js
@@ -1,56 +1,56 @@
-import sizes from "./sizes";
+import sizes from './sizes';
 
 const styles = {
-  Palette: {
-    height: "100vh",
-    overflowX: "hidden",
-    display: "flex",
-    flexDirection: "column"
-  },
-  PaletteColors: {
-    height: "90%"
-  },
-  goBack: {
-    width: "20%",
-    height: "50%",
-    margin: "0 auto",
-    display: "inline-block",
-    position: "relative",
-    cursor: "pointer",
-    marginBottom: "-3.5px",
-    opacity: 1,
-    backgroundColor: "black",
-    "& a": {
-      color: "white",
-      width: "100px",
-      height: "30px",
-      position: "absolute",
-      display: "inline-block",
-      top: "50%",
-      left: "50%",
-      marginLeft: "-50px",
-      marginTop: "-15px",
-      textAlign: "center",
-      outline: "none",
-      backgroundColor: "rgba(255, 255, 255, 0.3)",
-      fontSize: "1rem",
-      lineHeight: "30px",
-      textTransform: "uppercase",
-      border: "none",
-      textDecoration: "none"
-    },
-    [sizes.down("lg")]: {
-      width: "25%",
-      height: "33.3333%"
-    },
-    [sizes.down("md")]: {
-      width: "50%",
-      height: "20%"
-    },
-    [sizes.down("xs")]: {
-      width: "100%",
-      height: "10%"
-    }
-  }
+	Palette: {
+		height: '100vh',
+		// overflowX: "hidden",
+		display: 'flex',
+		flexDirection: 'column'
+	},
+	PaletteColors: {
+		height: '90%'
+	},
+	goBack: {
+		width: '20%',
+		height: '50%',
+		margin: '0 auto',
+		display: 'inline-block',
+		position: 'relative',
+		cursor: 'pointer',
+		marginBottom: '-3.5px',
+		opacity: 1,
+		backgroundColor: 'black',
+		'& a': {
+			color: 'white',
+			width: '100px',
+			height: '30px',
+			position: 'absolute',
+			display: 'inline-block',
+			top: '50%',
+			left: '50%',
+			marginLeft: '-50px',
+			marginTop: '-15px',
+			textAlign: 'center',
+			outline: 'none',
+			backgroundColor: 'rgba(255, 255, 255, 0.3)',
+			fontSize: '1rem',
+			lineHeight: '30px',
+			textTransform: 'uppercase',
+			border: 'none',
+			textDecoration: 'none'
+		},
+		[sizes.down('lg')]: {
+			width: '25%',
+			height: '33.3333%'
+		},
+		[sizes.down('md')]: {
+			width: '50%',
+			height: '20%'
+		},
+		[sizes.down('xs')]: {
+			width: '100%',
+			height: '10%'
+		}
+	}
 };
 export default styles;
