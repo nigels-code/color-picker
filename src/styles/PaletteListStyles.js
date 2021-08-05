26b583d3628b7cc8b87481b569dd7bf84cf3df8c
Nigel <75135800+nigels-code@users.noreply.github.com>
2021-08-05 20:07:46 +0100

    Add vertical scrollbar

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,59 +1,60 @@
-import sizes from "./sizes";
+import sizes from './sizes';
 
 const styles = {
-  "@global": {
-    ".fade-exit": {
-      opacity: 1
-    },
-    ".fade-exit-active": {
-      opacity: 0,
-      transition: "opacity 500ms ease-out"
-    }
-  },
-  root: {
-    backgroundColor: "blue",
-    height: "100vh",
-    display: "flex",
-    alignItems: "flex-start",
-    justifyContent: "center"
-    // fontSize: "1.6em"
-  },
-  container: {
-    width: "50%",
-    display: "flex",
-    alignItems: "flex-start",
-    flexDirection: "column",
-    flexWrap: "wrap",
-    [sizes.down("xl")]: {
-      width: "80%"
-    },
-    [sizes.down("xs")]: {
-      width: "75%"
-    }
-  },
-  nav: {
-    display: "flex",
-    width: "100%",
-    // fontSize: "0.8em",
-    justifyContent: "space-between",
-    alignItems: "center",
-    color: "white",
-    "& a": {
-      color: "white"
-    }
-  },
-  palettes: {
-    boxSizing: "border-box",
-    width: "100%",
-    display: "grid",
-    gridTemplateColumns: "repeat(3, 30%)",
-    gridGap: "2%",
-    [sizes.down("md")]: {
-      gridTemplateColumns: "repeat(2, 50%)"
-    },
-    [sizes.down("xs")]: {
-      gridTemplateColumns: "repeat(1, 100%)"
-    }
-  }
+	'@global': {
+		'.fade-exit': {
+			opacity: 1
+		},
+		'.fade-exit-active': {
+			opacity: 0,
+			transition: 'opacity 500ms ease-out'
+		}
+	},
+	root: {
+		backgroundColor: 'blue',
+		height: '100vh',
+		display: 'flex',
+		alignItems: 'flex-start',
+		justifyContent: 'center',
+		// fontSize: "1.6em"
+		overflow: 'scroll'
+	},
+	container: {
+		width: '50%',
+		display: 'flex',
+		alignItems: 'flex-start',
+		flexDirection: 'column',
+		flexWrap: 'wrap',
+		[sizes.down('xl')]: {
+			width: '80%'
+		},
+		[sizes.down('xs')]: {
+			width: '75%'
+		}
+	},
+	nav: {
+		display: 'flex',
+		width: '100%',
+		// fontSize: "0.8em",
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		color: 'white',
+		'& a': {
+			color: 'white'
+		}
+	},
+	palettes: {
+		boxSizing: 'border-box',
+		width: '100%',
+		display: 'grid',
+		gridTemplateColumns: 'repeat(3, 30%)',
+		gridGap: '2%',
+		[sizes.down('md')]: {
+			gridTemplateColumns: 'repeat(2, 50%)'
+		},
+		[sizes.down('xs')]: {
+			gridTemplateColumns: 'repeat(1, 100%)'
+		}
+	}
 };
 export default styles;
